9a67e847c787678b5c141cd87efadcd83a4726f5
SrLicht <francoquiroz_13@live.com>
2022-05-16 20:26:34 -0300

    Update Config.cs

--- UltimateAFK/Config.cs
+++ UltimateAFK/Config.cs
@@ -14,7 +14,7 @@ namespace UltimateAFK
         public bool CustomItemsSupport { get; set; } = false;
 
         [Description("If the number of players is less than this the plugin will not work.")]
-        public int MinPlayers { get; set; } = 8;
+        public int MinPlayers { get; set; } = 10;
 
         [Description("Tutorials should be ignored ?")]
         public bool IgnoreTut { get; set; } = true;
@@ -29,15 +29,15 @@ namespace UltimateAFK
         public int AfkCount { get; set; } = -1;
 
         [Description("When the player is detected as AFK and is in grace period this message will appear on his face. {0} represents the seconds the player has to move or be moved to spectator.")]
-        public string MsgGrace { get; set; } = "<color=white>[</color><color=green>Ultimate-AFK</color><color=white>]</color> <color=red>You will be moved to spectator if you do not move in less than <color=white>{0}</color> seconds.</color>";
+        public string MsgGrace { get; set; } = "<color=white>[</color><color=green>Ultimate-AFK</color><color=white>]</color> <color=red>Vas a ser movido a AFK en menos de  <color=white>{0}</color> segundos.</color>";
 
         [Description("This message will be sent to the player who has been moved to spectator when he is detected as AFK, it is also sent to the player's console.")]
-        public string MsgFspec { get; set; } = "<color=red>You were detected as AFK and were moved to spectator. WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP WAKE UP</color>";
+        public string MsgFspec { get; set; } = "<color=red>Fuiste detectado como AFK y fuiste movido al espectador.</color>";
 
         [Description("This will be the reason for the Kick, due to the VSR it is obligatory to clarify that it is a plugin with flags like [UltimateAFK] or something similar.")]
         public string MsgKick { get; set; } = "[Ultimate-AFK] You were removed from the server for being AFK for too long.!";
 
         [Description("When a player replaces another player, this message will appear on the player's face and on the player console.")]
-        public string MsgReplace { get; set; } = "<color=red> You replaced an AFK player.</color>";
+        public string MsgReplace { get; set; } = "<color=red> Reemplazaste a un jugador AFK.</color>";
     }
 }
